a919cbb3860f8a152437820030e7b3986c1a80d8
marcus-ao <mmarcusr@outlook.com>
2026-02-26 07:03:00 -0800

    新增第54题螺旋矩阵的解答与分析

---
+++ answers/01数组操作/3.二维数组/SpiralMatrix_54.py
@@ -0,0 +1,35 @@
+from typing import List
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])
+
+        top, bottom = 0, m - 1
+        left, right = 0, n - 1
+
+        result = []
+
+        while top <= bottom and left <= right:
+            for col in range(left, right + 1):
+                result.append(matrix[left][col])
+            
+            top += 1
+
+            for row in range(top, bottom + 1):
+                result.append(matrix[row][right])
+
+            right -= 1
+
+            if top <= bottom:
+                for col in range(right, left - 1, -1):
+                    result.append(matrix[bottom][col])
+
+            bottom -= 1
+
+            if left <= right:
+                for row in range(bottom, top - 1, -1):
+                    result.append(matrix[row][left])
+
+            left += 1
+
+        return result
