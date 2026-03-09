9527345f7e724e6592457b0b5feafd29ac7bdfcf
marcus-ao <mmarcusr@outlook.com>
2026-03-09 09:52:45 -0700

    新增第876题链表的中间节点的解答与分析

---
+++ answers/02双指针技巧/2.链表双指针/MiddleOfTheLinkedList_876.py
@@ -0,0 +1,19 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        fast = head
+        slow = head
+
+        while fast and fast.next:
+            assert slow is not None
+            fast = fast.next.next
+            slow = slow.next
+
+        return slow
+    
