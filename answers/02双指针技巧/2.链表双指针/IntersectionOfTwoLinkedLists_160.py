ffddbde08e5615cfe7184b082843a649a6a80e0e
marcus-ao <mmarcusr@outlook.com>
2026-03-10 23:50:56 -0700

    新增第160题寻找相交链表交点的解答与分析

---
+++ answers/02双指针技巧/2.链表双指针/IntersectionOfTwoLinkedLists_160.py
@@ -0,0 +1,17 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def getIntersectionNode(self, headA: Optional[ListNode], headB: Optional[ListNode]) -> Optional[ListNode]:
+        pA = headA
+        pB = headB
+
+        while pA != pB:
+            pA = pA.next if pA else headB
+            pB = pB.next if pB else headA
+        
+        return pB
